35b97e052573add8f8b8e9fbd82a7ecf72d54a99
Gabriel Ulick <gabrieludb@hotmail.com>
2023-10-31 19:42:37 -0300

    feat: get name desafio and favicon logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,32 @@ import "./styles.css";
 import api from "./services/api";
 
 function App() {
+  const [titulo, setTitulo] = useState("")
   const [inputUser, setInputUser] = useState("");
   const [inputEmail, setInputEmail] = useState("");
   const [inputPassword, setInputPassword] = useState("");
 
+  React.useEffect(() => {
+
+
+    const endPoint = '/';
+    // console.log(authData, "RENDER");
+
+    api.get(endPoint, {
+        validateStatus: status => {
+            return status < 405;
+        }
+    }).then(({ data }) => {
+        if (data.status === 404) {
+            // console.log('listaItem: ' + data.message);
+        } else {
+            console.log("aqui está dentro do DATA", data)
+            setTitulo(data)
+            // console.log("aqui está o tipoPV", TipoPv)
+        }  
+    });
+}, []);
+  
   const onSubmit = () => {
     const endPoint = "/singUp";
     const body = {
@@ -36,8 +58,9 @@ function App() {
 
   return (
     <div className="container">
+      <h1 className="title">{titulo.replace("<h1>","").replace("</h1>","")}</h1>
       <div className="boxCad">
-        <h1 className="title">Cadastro de Usuário</h1>
+        <h1 className="titleBox">Cadastro de usuairo</h1>
         <div className="inputs">
           <div className="containerInput">
             <input
